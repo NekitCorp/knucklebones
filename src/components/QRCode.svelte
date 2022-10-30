7b818221f5b53b0e88c4c46d8e7156b0b1384fe6
Nikita Madeev <n.madeev@softpro.com>
2022-10-30 14:14:57 +0300

    feat: :package: Change qr code generator library

--- src/components/QRCode.svelte
+++ src/components/QRCode.svelte
@@ -1,34 +1,11 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import QRious from 'qrious';
+    import QRCode from 'qrcode';
 
-    const qr = new QRious();
-
-    export let errorCorrection = 'L';
-    export let background = '#fff';
-    export let color = '#000';
-    export let size = '200';
     export let value: string;
-    export let padding = 0;
-    export let className = 'qrcode';
-
     let image = '';
 
-    function generateQrCode() {
-        qr.set({
-            background,
-            foreground: color,
-            level: errorCorrection,
-            padding,
-            size,
-            value,
-        });
-
-        image = qr.toDataURL('image/jpeg');
-    }
-
-    export function getImage() {
-        return image;
+    async function generateQrCode() {
+        image = await QRCode.toDataURL(value, { width: 200 });
     }
 
     $: {
@@ -36,19 +13,6 @@
             generateQrCode();
         }
     }
-
-    onMount(() => {
-        generateQrCode();
-    });
 </script>
 
-<div class="wrapper">
-    <img src={image} alt={value} class={className} />
-</div>
-
-<style>
-    .wrapper {
-        background: #fff;
-        padding: 4px;
-    }
-</style>
+<img src={image} alt={value} />
