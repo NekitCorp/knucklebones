2c40cfaac49b98dd507d6b0879b245dd4bfc80f8
Nikita Madeev <n.madeev@softpro.com>
2022-10-31 09:09:45 +0300

    feat: :lipstick: Add color scheme to qr code

--- src/components/QRCode.svelte
+++ src/components/QRCode.svelte
@@ -1,18 +1,46 @@
 <script lang="ts">
     import QRCode from 'qrcode';
+    import { onDestroy, onMount } from 'svelte';
+
+    // Props
 
     export let value: string;
+    export let width: number = 200;
+
+    // Variables
+
     let image = '';
+    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Functions
+
+    function getColors(isDark: boolean) {
+        return isDark ? { dark: '#ffffffde', light: '#2f2f2fff' } : { dark: '#000000ff', light: '#ffffffff' };
+    }
 
     async function generateQrCode() {
-        image = await QRCode.toDataURL(value, { width: 200 });
+        image = await QRCode.toDataURL(value, { width: width, color: getColors(mediaQueryList.matches) });
     }
+
+    function onColorSchemeChange(e: MediaQueryListEvent) {
+        generateQrCode();
+    }
+
+    // Lifecycle
 
     $: {
         if (value) {
             generateQrCode();
         }
     }
+
+    onMount(() => {
+        mediaQueryList.addEventListener('change', onColorSchemeChange);
+    });
+
+    onDestroy(() => {
+        mediaQueryList.removeEventListener('change', onColorSchemeChange);
+    });
 </script>
 
 <img src={image} alt={value} />
